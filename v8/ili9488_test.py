c9a422bfa98cdc14f880745786d7cb34fcca88b3
iotah <writeforever@foxmail.com>
2024-04-18 19:33:54 +0800

    add qd3503728 v8 example
    Signed-off-by: iotah <writeforever@foxmail.com>

---
+++ v8/ili9488_test.py
@@ -0,0 +1,62 @@
+# init
+
+import usys as sys
+sys.path.append('') # See: https://github.com/micropython/micropython/issues/6419
+
+import lvgl as lv
+import lv_utils
+
+lv.init()
+
+class driver:
+    def init_gui(self):
+        import ili9488 as tft
+        import ft6236 as tp
+
+        hres = 480
+        vres = 320
+
+        # Register display driver
+        event_loop = lv_utils.event_loop()
+        tft.deinit()
+        tft.init()
+        tp.init()
+
+        disp_buf1 = lv.disp_draw_buf_t()
+        buf1_1 = tft.framebuffer(1)
+        buf1_2 = tft.framebuffer(2)
+        disp_buf1.init(buf1_1, buf1_2, len(buf1_1) // lv.color_t.__SIZE__)
+        disp_drv = lv.disp_drv_t()
+        disp_drv.init()
+        disp_drv.draw_buf = disp_buf1
+        disp_drv.flush_cb = tft.flush
+        # disp_drv.gpu_blend_cb = tft.gpu_blend
+        # disp_drv.gpu_fill_cb = tft.gpu_fill
+        disp_drv.hor_res = hres
+        disp_drv.ver_res = vres
+        disp_drv.register()
+
+        # Register touch sensor
+        indev_drv = lv.indev_drv_t()
+        indev_drv.init()
+        indev_drv.type = lv.INDEV_TYPE.POINTER
+        indev_drv.read_cb = tp.ts_read
+        indev_drv.register()
+
+if not lv_utils.event_loop.is_running():
+    drv = driver()
+    drv.init_gui()
+
+############################################################################################
+
+scr = lv.obj()
+btn = lv.btn(scr)
+btn.align(lv.ALIGN.CENTER, 0, 0)
+label = lv.label(btn)
+label.set_text('Hello World!')
+
+lbl = lv.label(scr)
+lbl.set_text("Present by embeddedboys")
+lbl.align(lv.ALIGN.BOTTOM_MID, 0, -20)
+
+lv.scr_load(scr)
