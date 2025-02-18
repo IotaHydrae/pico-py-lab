cf45d962d78b173a1a77d7c67d56e2a5dd49a9bb
Zheng Hua <hua.zheng@embeddedboys.com>
2025-02-19 05:36:07 +0800

    update LS013B7DH06 add color fill support
    Signed-off-by: Zheng Hua <hua.zheng@embeddedboys.com>

--- LS013B7DH06.py
+++ LS013B7DH06.py
@@ -6,46 +6,98 @@ from machine import Pin, SPI
 TFT_HOR_RES = 128   # around to 216 bits in a row actually
 TFT_VER_RES = 128
 
+BLACK   = 0x00
+RED     = 0x80
+GREEN   = 0x40
+YELLOW  = 0xC0
+BLUE    = 0x20
+MAGENTA = 0xA0
+CYAN    = 0x60
+WHITE   = 0xE0
+
 OFFSET_X = 0
 OFFSET_Y = 0
 
-g_tft_buf = [0x0] * int(25 * 200)
+# g_tft_buf = [0x0] * int(25 * 200)
 
 class tft(object):
     def __init__(self):
-        self.spi = SPI(0, baudrate=5000000, sck=Pin(18), mosi=Pin(19))
-        self.mod = Pin(15, mode=Pin.OUT, value=1)
-        self.inv = Pin(14, mode=Pin.OUT, value=1)
+        # self.spi = SPI(0, baudrate=1000000, sck=Pin(18), mosi=Pin(19))
+        self.sck = Pin(18, mode=Pin.OUT, value=0)
+        self.mosi = Pin(19, mode=Pin.OUT, value=0)
+        self.mod = Pin(15, mode=Pin.OUT, value=0)
+        self.inv = Pin(14, mode=Pin.OUT, value=0)
         self.cs = Pin(13, mode=Pin.OUT, value=0)
-        self.blk = Pin(12, mode=Pin.OUT, value=1)
+        self.blk = Pin(12, mode=Pin.OUT, value=0)
 
-    def spi_write8(self, b):
-        self.spi.write(bytearray([b]))
-        # print(b)
+    # def spi_write8(self, b):
+    #     self.spi.write(bytearray([b]))
+    #     # print(b)
 
-    def write_byte(self, cmd):
-        self.cs.low()
-        self.spi.write(bytearray([cmd]))
-        self.cs.high()
-        # self.spi.write(bytearray([cmd]))
+    # def write_byte(self, val):
+    #     self.spi_write8(val)
+
+    def spi_writen(self, val, n):
+        for i in range(n):
+            self.sck.low()
+            if val & 0x80:
+                self.mosi.high()
+            else:
+                self.mosi.low()
+            self.sck.high()
+            val <<= 1
+        self.mosi.low()
+        self.sck.low()
+
+    def write_bits(self, val, n):
+        self.spi_writen(val, n)
 
     def init_display(self):
+        # This driver IC does not have any
+        # register for setting the display
         pass
 
+    # see manual page 20, 6-6-4 All Clear Mode
     def clear(self):
-        self.write_byte
+        # self.cs.low()
+        self.cs.high()
 
-    def put_pixel(self, x, y, color):
-        ca_slice = int(x / 4)
-        addr = int(y / 2) * 48 + ca_slice
-        print(x, ca_slice, addr)
-        if y % 2 != 0:
-            g_tft_buf[addr] |= (0x80 >> (2 * (x % 4) - 2))
-        else:
-            # TODO: add even rows buffer drawing
-            pass
+        self.write_bits(0x20, 6)
+        self.write_bits(0x00, 2)
+        self.write_bits(0x00, 8)
+
+        self.cs.low()
+
+    def fill_color(self, color):
+        # self.cs.low()
+        self.cs.high()
+
+        self.write_bits(0x80, 6)
+
+        for i in range(256):
+            self.write_bits(0x00, 2)
+            self.write_bits(i, 8)
+
+            for i in range(128):
+                self.write_bits(color, 3)
+            self.write_bits(0x00, 6)
+
+        self.write_bits(0x00, 2)
+        self.write_bits(0x00, 8)
+
+        self.cs.low()
+        self.cs.high()
 
     def flush(self):
+        self.cs.low()
+        self.cs.high()
+        self.write_bits(0x00, 6)
+        self.write_bits(0x00, 2)
+        self.write_bits(0x00, 8)
+
+        self.cs.low()
+
+    def put_pixel(self, x, y, color):
         pass
 
 if __name__ == "__main__":
@@ -54,17 +106,33 @@ if __name__ == "__main__":
 
     dev = tft()
     dev.init_display()
-    
-    while True:
-        dev.inv.low()
-        time.sleep_ms(200)
-        dev.inv.high()
-        time.sleep_ms(200)
-        dev.blk.low()
-        time.sleep_ms(200)
-        dev.blk.high()
-        time.sleep_ms(200)
 
-    print("flushing...")
-    dev.flush()
-    print("done.")
+    dev.blk.low()
+    dev.clear()
+    time.sleep_ms(5)
+    dev.blk.high()
+
+    while True:
+        dev.fill_color(BLACK)
+        time.sleep_ms(500)
+
+        dev.fill_color(RED)
+        time.sleep_ms(500)
+
+        dev.fill_color(GREEN)
+        time.sleep_ms(500)
+
+        dev.fill_color(YELLOW)
+        time.sleep_ms(500)
+
+        dev.fill_color(BLUE)
+        time.sleep_ms(500)
+
+        dev.fill_color(MAGENTA)
+        time.sleep_ms(500)
+
+        dev.fill_color(CYAN)
+        time.sleep_ms(500)
+
+        dev.fill_color(WHITE)
+        time.sleep_ms(500)
