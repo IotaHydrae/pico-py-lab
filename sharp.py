2a7f954175908f78e2ef6041980f55931a7679a6
Zheng Hua <hua.zheng@embeddedboys.com>
2025-04-01 11:10:04 +0800

    update sharp LS013B7DH06 example
    Signed-off-by: Zheng Hua <hua.zheng@embeddedboys.com>

--- sharp.py
+++ sharp.py
@@ -12,25 +12,34 @@ MAGENTA = 0x05
 CYAN    = 0x03
 WHITE   = 0x07
 
+SUPPORTED_COLORS = [
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    MAGENTA,
+    CYAN,
+    WHITE
+]
 
+
+def bitrev8(n):
+    n = ((n & 0b11110000) >> 4) | ((n & 0b00001111) << 4)
+    n = ((n & 0b11001100) >> 2) | ((n & 0b00110011) << 2)
+    n = ((n & 0b10101010) >> 1) | ((n & 0b01010101) << 1)
+    return n
 
 class LS013B7DH06(object):
     def __init__(self):
-        self.spi = SPI(0, baudrate=(25 * 1000 * 1000), sck=Pin(18), mosi=Pin(19))
+        self.spi = SPI(0, baudrate=(20 * 1000 * 1000), sck=Pin(18), mosi=Pin(19))
         self.mod = Pin(15, mode=Pin.OUT, value=0)
         self.inv = Pin(14, mode=Pin.OUT, value=0)
-        self.cs = Pin(13, mode=Pin.OUT, value=0)
-        self.blk = Pin(12, mode=Pin.OUT, value=0)
+        self.cs = Pin(13, mode=Pin.OUT, value=0)    # Active high
+        self.disp = Pin(12, mode=Pin.OUT, value=0)   # disp
         self.width = 128
         self.height = 128
         self.line_width = self.width * 3 / 8
         self.framebuffer = [0xff] * int(self.width * self.line_width)
-
-    def reverse_byte(self, n):
-        n = ((n & 0b11110000) >> 4) | ((n & 0b00001111) << 4)
-        n = ((n & 0b11001100) >> 2) | ((n & 0b00110011) << 2)
-        n = ((n & 0b10101010) >> 1) | ((n & 0b01010101) << 1)
-        return n
 
     def spi_write8(self, b):
         b = b.to_bytes(1, "little")
@@ -51,11 +60,10 @@ class LS013B7DH06(object):
 
         # 1 pixel cost 3 bit, so see 8 pixels as a group (3 byte)
         line_buffer = self.framebuffer[y * 48: (y + 1) * 48]
-        # print(self.framebuffer[y * 48: (y + 1) * 48])
         # find out the x in which group
         index = int(x * 3 / 24)
         offs = ((x * 3) % 24)
-        # print(line_buffer[index * 3: index * 3 + 3])
+
         group = int.from_bytes(bytes(line_buffer[index * 3: index * 3 + 3]), "little")
         group &= ~(0b111 << offs)
         group |= (color << offs)
@@ -65,7 +73,6 @@ class LS013B7DH06(object):
         line_buffer[index * 3 + 2] = (group >> 16) & 0xff
 
         self.framebuffer[y * 48: (y + 1) * 48] = line_buffer
-        # print(self.framebuffer[y * 48: (y + 1) * 48])
         pass
 
 
@@ -83,10 +90,10 @@ class LS013B7DH06(object):
         for i in range(0, self.height):
             self.spi_write8(0x00)   # 8 dummy clock
             addr = i + 1
-            self.spi_write8(self.reverse_byte(addr))      # current gate line addr
+            self.spi_write8(bitrev8(addr))      # current gate line addr
 
             for k in range(48):
-                self.spi_write8(self.reverse_byte(self.framebuffer[i * 48 + k]))
+                self.spi_write8(bitrev8(self.framebuffer[i * 48 + k]))
 
         # 16 dummy clock
         self.spi_write8(0x00)
@@ -95,17 +102,8 @@ class LS013B7DH06(object):
         self.cs.low()
         self.cs.high()
 
-        lcd.blk.low()
-        lcd.blk.high()
-
-    def display(self):
-        self.cs.high()
-
-        self.spi_write8(0x00)
-        self.spi_write8(0x00)
-
-        self.cs.low()
-        self.cs.high()
+        lcd.disp.low()
+        lcd.disp.high()
 
 if __name__ == "__main__":
     machine.freq(240000000)  # set the CPU frequency to 240 MHz
@@ -114,17 +112,13 @@ if __name__ == "__main__":
     lcd = LS013B7DH06()
     lcd.clear()
 
-    # for i in range(256):
-    #     lcd.update_line(i, BLACK)
+    print("Framebuffer size:", len(lcd.framebuffer), "bytes")
 
-    print("Framebuffer size:", len(lcd.framebuffer))
+    while True:
+        for color in SUPPORTED_COLORS:
 
-    for x in range(64):
-        lcd.set_pixel(x + 0, 64, GREEN)
-        lcd.set_pixel(x + 0, 100, GREEN)
+            for x in range(128):
+                for y in range(128):
+                    lcd.set_pixel(x, y, color)
 
-    for x in range(128):
-        for y in range(128):
-            lcd.set_pixel(x, y, GREEN)
-
-    lcd.refresh()
+            lcd.refresh()
